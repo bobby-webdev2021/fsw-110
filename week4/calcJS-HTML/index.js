bf6276eb1cd9d09cc3c9add5fc56bef3a0093126
bobby-webdev2021 <85532644+bobby-webdev2021@users.noreply.github.com>
2021-08-24 21:55:08 -0500

    Add files via upload

---
+++ week4/calcJS-HTML/index.js
@@ -0,0 +1,28 @@
+function addition (num1, num2){
+    return(num1+num2);
+}
+
+function subtraction (num1, num2){
+    return(num1-num2);
+}
+
+function multiplication (num1, num2){
+    return(num1*num2);
+}
+
+
+document.getElementById("btnadd").addEventListener("click", function(){
+    document.getElementById("answer").textContent = addition(Number(document.getElementById("firstnum").value), Number(document.getElementById("secondnum").value));
+})
+
+document.getElementById("btnsub").addEventListener("click", function(){
+    document.getElementById("answer2").textContent = subtraction(Number(document.getElementById("firstnum2").value), Number(document.getElementById("secondnum2").value));
+})
+
+document.getElementById("btnmult").addEventListener("click", function(){
+    document.getElementById("answer3").textContent = multiplication(Number(document.getElementById("firstnum3").value), Number(document.getElementById("secondnum3").value));
+})
+
+
+
+
