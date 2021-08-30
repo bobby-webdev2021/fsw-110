a762be43b55e55656fa24682cbe000d8cd6e6283
bobby-webdev2021 <85532644+bobby-webdev2021@users.noreply.github.com>
2021-08-30 13:43:11 -0500

    Add files via upload

---
+++ week5/otherTypeofHTML/index.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var first = document.getElementById("name").value
+    var last = document.getElementById("last").value
+    var age = document.getElementById("age").value
+    var radbut = document.getElementById("radbut").value
+    alert("First Name: "+ first + "\n" + "Last Name: " + last + "\n" + "Age: " + age  + "\n" + "answer: " + radbut);
+  }
+  
---
+++ week5/pnp-modEleValue/index.js
@@ -0,0 +1,10 @@
+function myFunction(){
+  var first = document.getElementById("name").value
+  var last = document.getElementById("last").value
+  var age = document.getElementById("age").value
+  alert("First Name: "+ first + "\n" + "Last Name: " +last + "\n" + "Age: " + age);
+}
+
+
+
+
